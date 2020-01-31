de4349f24a263a15a9b4db2d7c1fbd535d4ff509
lykh <llj2yx@gmail.com>
2020-02-01 00:06:01 +0800

    update

--- shell/cl.sh
+++ shell/cl.sh
@@ -33,23 +33,30 @@ else
 	cd $da
 fi
 
+tou="https:\/\/cl.nvgm.icu\/"
+mda=`date +%y%m`
+mulu="htm_data/$mda"
+if [ ! -f /samba/llj/htm$mda ];then
+touch /samba/llj/htm$mda
+fi
+if [ ! -f /samba/llj/jpg$mda ];then
+touch /samba/llj/jpg$mda
+fi
+
 if [[ -n $1 && $1 = 'd' ]];then
 curl https://cl.nvgm.icu/thread0806.php?fid=16 >/tmp/xxx1
 else
 curl https://cl.nvgm.icu/thread0806.php?fid=8 >/tmp/xxx1
 fi
-
-tou="https:\/\/cl.nvgm.icu\/"
-mulu="htm_data/2001"
 egrep -o "<a href[^>]+>" /tmp/xxx1 | egrep -o "$mulu.*.html" |sort | uniq >/tmp/xxx2
 sed -i  "s/^/$tou/g"  /tmp/xxx2
 #cat /tmp/xxx2 | sed  "s/^/$tou/g" > /tmp/xxx3
 
 while read line;do
-grep "$line" /samba/llj/xxxx
+grep "$line" /samba/llj/htm$mda
 if [ $? -ne 0 ];then
 curl $line >/tmp/xx4
-echo $line >>/samba/llj/xxxx
+echo $line >>/samba/llj/htm$mda
 else
 	continue
 fi
@@ -58,16 +65,16 @@ egrep -o "<img[^>]+>" /tmp/xx4 | egrep -o "http[^(http)]*(.jpg|.JPG|.png|.PNG|.j
 sed -i '/?/d' /tmp/xx5
 #cat /tmp/xx5 >> /samba/llj/xxx
 while read line;do
-grep "$line" /samba/llj/xxx
+grep "$line" /samba/llj/jpg$mda
 if [ $? -ne 0 ];then
-echo $line >>/samba/llj/xxx
+echo $line >>/samba/llj/jpg$mda
 let bb=$RANDOM%23+1
 wget -t1 -T1 --user-agent="${agent[$bb]}" $line
 fi
 done < /tmp/xx5
 
 done < /tmp/xxx2
-sudo chmod 666 *
+sudo chmod 666 * 2>/dev/null
 du -h
 echo ok-----------------
 exit
