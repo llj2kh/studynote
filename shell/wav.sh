97e692feb7c983a90fdaa14743e64a0546224e64
lykh <llj2yx@gmail.com>
2020-03-09 20:16:53 +0800

    update

--- shell/wav.sh
+++ shell/wav.sh
@@ -1,10 +1,47 @@
 #!/bin/bash
 
-cd /samba/DLNA/Music/wav
-for mu in *.wav;do
-	aplay -D bluealsa:DEV=88:00:00:00:03:E5,PROFILE=A2DP $mu
+
+stop(){
+	pid=`ps -e|grep aplay |awk '{print $1}'`
+	if [ -n "$pid" ];then
+		kill -9 $pid
+	fi
+}
+
+start(){
+path=/samba/DLNA/Music/wav/
+count=0
+while true;do
+	if [ $count -eq 0 ];then
+		ls $path >/tmp/ww
+		[ -n $mu ] && sed -i "/${mu}/d" /tmp/ww
+		count=`cat /tmp/ww |wc -l `
+	fi
+	let num=$RANDOM%$count+1
+	mu=`sed -n "${num}p" /tmp/ww`
+	aplay -D bluealsa:DEV=88:00:00:00:03:E5,PROFILE=A2DP $path$mu
+#	echo "Playing the $mu song"
 	if [ $? -ne 0 ]; then
 		exit 3
 	fi
+	let count-=1
+	sed -i "${num}d" /tmp/ww
 done
+}
+
+
+
+case $1 in
+	start)
+		stop
+		start
+		;;
+	stop)
+		stop
+		;;
+	*)
+		stop
+		start
+		;;
+esac
 
